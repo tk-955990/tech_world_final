f8a81a95505caa3433b5254fc4301b17b4a73ec2
tk-955990 <105995713+tk-955990@users.noreply.github.com>
2022-08-14 23:24:04 +0900

    kadai_final_2022/08/14/2323

--- kadai_final/Tool.java
+++ kadai_final/Tool.java
@@ -7,7 +7,6 @@ public class Tool {
 		System.out.println("   A   B   C   D   E   F   G   H");
 		System.out.println(" ┏━┳━┳━┳━┳━┳━┳━┳━┓");
 
-
 		for(int j = 0;j<board.length;j++) {
 			System.out.print((j+1)+"┃");
 			for(int i = 0;i<8;i++) {
@@ -49,15 +48,13 @@ public class Tool {
 		}
 		System.out.println("黒: "+countB+"個");
 		System.out.println("白: "+countW+"個");
-		
-
 	}
 
 	// コマをひっくり返す処理
 	public void turnStone(int a, int b, int stoneColor) {
 		turnLeftUp   (a, b, stoneColor);     //左上
 		turnUp       (a, b, stoneColor);
-	 	turnRightUp  (a, b, stoneColor);
+		turnRightUp  (a, b, stoneColor);
 		turnRight    (a, b, stoneColor);
 		turnRightDown(a, b, stoneColor);
 		turnDown     (a, b, stoneColor);
@@ -65,197 +62,219 @@ public class Tool {
 		turnLeft     (a, b, stoneColor);
 	}
 
-	public void turnLeftUp(int a, int b,int stoneColor) {
-		// Comが置こうとしてるマスが空であるかの条件分岐
-		if(main.board[a][b] == 0) {
-			if (b > 1 && a > 1) {
+	public int turnLeftUp(int a, int b,int stoneColor) {
+		
+		int turnCount = 0;
+        
+		// 隣にコマが二個以上あるかの判定
+		if (b > 1 && a > 1) {
+			// 隣のコマ
+			int  next = main.board[a-1][b-1];
+			// 隣のコマが相手の色だった場合
+			if((next != stoneColor)&&(next != 0)) {
+				for(int i = 2;true;i++) {
 
-				//隣のコマ
-				int  next = main.board[a-1][b-1];
-				//隣のコマが相手の色だった場合
-				if((next != stoneColor)&&(next != 0)) {
-					for(int i = 2;true;i++) {
-
-						if (a - i < 0 || b - i < 0 || main.board[a - i][b - i]== 0) {
-							//コマが無かった場合終了
-							break;
-						} else if (main.board[a - i][b - i]== stoneColor) {
-							//コマが自分の色だった場合
-							//間のコマをひっくり返す
-							for (int t = 1; t < i; t++) {
-								// 配列の要素を上書き
-								main.board[a - t][b - t] *= -1;
-								main.board[a][b] = stoneColor;
-							}
-							break;
+					if (a - i < 0 || b - i < 0 || main.board[a - i][b - i]== 0) {
+						
+						// コマが無かった場合終了
+						break;
+					} else if (main.board[a - i][b - i]== stoneColor) {
+						
+						// コマが自分の色だった場合
+						// 間のコマをひっくり返す
+						for (int t = 1; t < i; t++) {
+						
+							// 配列の要素を上書き
+							main.board[a - t][b - t] *= -1;
+							main.board[a][b] = stoneColor;
+							turnCount++;  // ひっくり返したカウント
 						}
+						break;
 					}
 				}
 			}
 		}
+		return turnCount;
 	}
 
-	public void turnUp(int a, int b, int stoneColor) {
-		if(main.board[a][b] == 0) {
+	public int turnUp(int a, int b, int stoneColor) {
+		
+		int turnCount = 0;
+		
+		if (b > 1) {
 
-			if (b > 1) {
+			int  next = main.board[a][b-1];
 
-				int  next = main.board[a][b-1];
-
-				if((next != stoneColor)&&(next != 0)) {
-					for(int i = 2;true;i++) {
-						if(b - i < 0 || main.board[a][b-i] == 0) {
-							break;
-						}else if(main.board[a][b - i] == stoneColor) {
-							for (int t = 1; t < i; t++) {
-								main.board[a][b - t] *= -1;
-								main.board[a][b] = stoneColor;
-							}
-							break;
+			if((next != stoneColor)&&(next != 0)) {
+				for(int i = 2;true;i++) {
+					if(b - i < 0 || main.board[a][b-i] == 0) {
+						break;
+					}else if(main.board[a][b - i] == stoneColor) {
+						for (int t = 1; t < i; t++) {
+							main.board[a][b - t] *= -1;
+							main.board[a][b] = stoneColor;
+							turnCount++;
 						}
+						break;
 					}
 				}
 			}
 		}
+		return turnCount;
 	}
 
-	public void turnRightUp(int a, int b, int stoneColor) {
-		if(main.board[a][b] == 0) {
+	public int turnRightUp(int a, int b, int stoneColor) {
+		int turnCount = 0;
 
-			if (a < 6 && b> 1) {
+		if (a < 6 && b> 1) {
 
-				int  next = main.board[a+1][b-1];
+			int  next = main.board[a+1][b-1];
 
-				if((next != stoneColor)&&(next != 0)) {
-					for(int i = 2;true;i++) {
-						if(a + i < 0 || b - i < 0 || main.board[a+i][b-i] == 0) {
-							break;
-						}else if(main.board[a + i][b - i] == stoneColor) {
-							for(int t = 1; t < i;t++) {
-								main.board[a+t][b-t] *= -1;
-								main.board[a][b] = stoneColor;
-							}
-							break;
+			if((next != stoneColor)&&(next != 0)) {
+				for(int i = 2;true;i++) {
+					if(a + i > 7|| b - i < 0 || main.board[a+i][b-i] == 0) {
+						break;
+					}else if(main.board[a + i][b - i] == stoneColor) {
+						for(int t = 1; t < i;t++) {
+							main.board[a+t][b-t] *= -1;
+							main.board[a][b] = stoneColor;
+							turnCount++;
 						}
+						break;
 					}
 				}
 			}
 		}
+		return turnCount;
 	}
 
-	public void turnRight(int a, int b, int stoneColor) {
-		if(main.board[a][b] == 0) {
-			if (a < 6) {
+	public int turnRight(int a, int b, int stoneColor) {
 
-				int  next = main.board[a+1][b];
+		int turnCount = 0;
 
-				if((next != stoneColor)&&(next != 0)) {
-					for(int i = 2;true;i++) {
-						if (a + i < 0 || main.board[a + i][b]== 0) {
-							break;
-						} else if (main.board[a + i][b]== stoneColor) {
-							for (int t = 1; t < i; t++) {
-								main.board[a + t][b] *= -1;
-								main.board[a][b] = stoneColor;
-							}
-							break;
+		if (a < 6) {
+
+			int  next = main.board[a+1][b];
+
+			if((next != stoneColor)&&(next != 0)) {
+				for(int i = 2;true;i++) {
+					if (a + i > 7 || main.board[a + i][b]== 0) {
+						break;
+					} else if (main.board[a + i][b]== stoneColor) {
+						for (int t = 1; t < i; t++) {
+							main.board[a + t][b] *= -1;
+							main.board[a][b] = stoneColor;
+							turnCount++;
 						}
+						break;
 					}
 				}
 			}
 		}
+		return turnCount;
 	}
 
-	public void turnRightDown(int a, int b, int stoneColor) {
-		if(main.board[a][b] == 0) {
-			if (b < 6 && a < 6) {
+	public int turnRightDown(int a, int b, int stoneColor) {
 
-				int  next = main.board[a+1][b+1];
+		int turnCount = 0;
 
-				if((next != stoneColor)&&(next != 0)) {
-					for(int i = 2;true;i++) {
-						if(a + i < 0 || b + i < 0 || main.board[a+i][b+i] == 0) {
-							break;
-						}else if(main.board[a + i][b + i] == stoneColor) {
-							for(int t = 1; t < i;t++) {
-								main.board[a+t][b+t] *= -1;
-								main.board[a][b] = stoneColor;
+		if (b < 6 && a < 6) {
 
-							}
-							break;
+			int  next = main.board[a+1][b+1];
+
+			if((next != stoneColor)&&(next != 0)) {
+				for(int i = 2;true;i++) {
+					if(a + i > 7 || b + i > 7 || main.board[a+i][b+i] == 0) {
+						break;
+					}else if(main.board[a + i][b + i] == stoneColor) {
+						for(int t = 1; t < i;t++) {
+							main.board[a+t][b+t] *= -1;
+							main.board[a][b] = stoneColor;
+							turnCount++;
 						}
+						break;
 					}
 				}
 			}
 		}
+		return turnCount;
 	}
 
-	public void turnDown(int a, int b, int stoneColor) {
-		if(main.board[a][b] == 0) {
-			if (b < 6) {
+	public int turnDown(int a, int b, int stoneColor) {
 
-				int  next = main.board[a][b+1];
+		int turnCount = 0;
 
-				if((next != stoneColor)&&(next != 0)) {
-					for(int i = 2;true;i++) {
-						if(b + i < 0 || main.board[a][b+i] == 0) {
-							break;
-						}else if(main.board[a][b + i] == stoneColor) {
-							for (int t = 1; t < i; t++) {
-								main.board[a][b + t] *= -1;
-								main.board[a][b] = stoneColor;
-							}
-							break;
+		if (b < 6) {
+
+			int  next = main.board[a][b+1];
+
+			if((next != stoneColor)&&(next != 0)) {
+				for(int i = 2;true;i++) {
+					if(b + i > 7 || main.board[a][b+i] == 0) {
+						break;
+					}else if(main.board[a][b + i] == stoneColor) {
+						for (int t = 1; t < i; t++) {
+							main.board[a][b + t] *= -1;
+							main.board[a][b] = stoneColor;
+							turnCount++;
 						}
+						break;
 					}
 				}
 			}
 		}
+		return turnCount;
 	}
 
-	public void turnLeftDown(int a, int b, int stoneColor) {
-		if(main.board[a][b] == 0) {
-			if (b < 6 && a > 1) {
+	public int turnLeftDown(int a, int b, int stoneColor) {
 
-				int  next = main.board[a-1][b+1];
+		int turnCount = 0;
 
-				if((next != stoneColor)&&(next != 0)) {
-					for(int i = 2;true;i++) {
-						if(a - i < 0 || b + i < 0 || main.board[a-i][b+i] == 0) {
-							break;
-						}else if(main.board[a - i][b + i] == stoneColor) {
-							for(int t = 1; t < i;t++) {
-								main.board[a-t][b+t] *= -1;
-								main.board[a][b] = stoneColor;
-							}
-							break;
+		if (b < 6 && a > 1) {
+
+			int  next = main.board[a-1][b+1];
+
+			if((next != stoneColor)&&(next != 0)) {
+				for(int i = 2;true;i++) {
+					if(a - i < 0 || b + i > 7 || main.board[a-i][b+i] == 0) {
+						break;
+					}else if(main.board[a - i][b + i] == stoneColor) {
+						for(int t = 1; t < i;t++) {
+							main.board[a-t][b+t] *= -1;
+							main.board[a][b] = stoneColor;
+							turnCount++;
 						}
+						break;
 					}
 				}
 			}
 		}
+		return turnCount;
 	}
 
-	public void turnLeft(int a, int b, int stoneColor) {
-		if(main.board[a][b] == 0) {
-			if (a > 1) {
+	public int turnLeft(int a, int b, int stoneColor) {
 
-				int  next = main.board[a-1][b];
+		int turnCount = 0;
 
-				if((next != stoneColor)&&(next != 0)) {
-					for(int i = 2;true;i++) {
-						if (a - i < 0 || main.board[a - i][b]== 0) {
-							break;
-						} else if (main.board[a - i][b]== stoneColor) {
-							for (int t = 1; t < i; t++) {
-								main.board[a - t][b] *= -1;
-								main.board[a][b] = stoneColor;
-							}
-							break;
+		if (a > 1) {
+
+			int  next = main.board[a-1][b];
+
+			if((next != stoneColor)&&(next != 0)) {
+				for(int i = 2;true;i++) {
+					if (a - i < 0 || main.board[a - i][b]== 0) {
+						break;
+					} else if (main.board[a - i][b]== stoneColor) {
+						for (int t = 1; t < i; t++) {
+							main.board[a - t][b] *= -1;
+							main.board[a][b] = stoneColor;
+							turnCount++;
 						}
+						break;
 					}
 				}
 			}
 		}
+		return turnCount;
 	}
 }
--- kadai_final/main.java
+++ kadai_final/main.java
@@ -6,8 +6,6 @@ import java.util.Scanner;
 public class main {
 
 	static int[][] board = new int[8][8];
-    static int stoneColor; 
-
 
 	/*	{{0,0,0,0,0,0,0,0},
 			{0,0,0,0,0,0,0,0},
@@ -19,7 +17,6 @@ public class main {
 			{0,0,0,0,0,0,0,0}}; */ 
 	static InputValueJudgment ivj = new InputValueJudgment();
     static Tool tool = new Tool();
-    
 	public static void main (String[] args) {
 
 		board[3][3] = 1;
@@ -36,95 +33,128 @@ public class main {
 
 	public static void game(int[][] board) {
 
-		System.out.println("********** Game Start!! **********");
-
+		System.out.println("********** Ga me Start!! **********");
 
 		Scanner scanner = new Scanner(System.in);
 
-		int checkValue; // 入力値のチェック[ 0:エラー　1:先攻　2:後攻 ]
-		
-		while(true) {
+		int checkValue = 0; // 入力値のチェック[ 0:エラー　1:先攻　2:後攻 ]
+		int stoneColor = 0 ; 
+		int turnFlag = 0;
+		int turnCount = 0;
+
+		while(checkValue ==0) {
 			System.out.println("【　先攻、後攻を決定してください　】");
 			System.out.println("【　先攻：1　　後攻：2　】");
 
 			String turnInput = scanner.next();
 
 			checkValue = ivj.convertInputTurn(turnInput);
-			if(checkValue != 0) {
+			
+			switch (checkValue) {
+			case 1: 
+				turnFlag = 1;
+				break;
+			case 2:
+				turnFlag = 2;
 				break;
 			}
-			
-		}
+			System.out.println("checkValue="+checkValue);
+			System.out.println("turnFlag 1="+turnFlag);
 
-		while(true) {
+		}
 			// 後攻の場合
-			if(checkValue == 2) {
-				for(int i = 0;i < 63;i++) {
-					
+		while (true) {
+			int a = 0;
+			int b = 0;
+
+			if(turnFlag %2 == 0) {
+
+				for(int i = 0;i < 64;i++) {
+					int count = 0 ; 
+
 					Random random1  = new Random();
 					int randomValue = random1.nextInt(7);
 					Random random2  = new Random();
 					randomValue     = random2.nextInt(7);
 
-					int a = random1.nextInt(7);
-					int b = random2.nextInt(7);  
-						
-					if(checkValue == 1) {
-						stoneColor  = 1;
+					a = random1.nextInt(7);
+					b = random2.nextInt(7);  
+
+					if(checkValue == 1) {  // 後攻
+						stoneColor  = 1;   // コマの色　黒
 					}else {
-						stoneColor  = -1;
+						stoneColor  = -1;  //           白
 					}
 
-					
-					
 					// デバッグコード
-					System.out.println("乱数X = "+ a);
-					System.out.println("乱数Y = "+ b);
-					
+					System.out.println("乱数X = "+i+"回目"+ a);
+					System.out.println("乱数Y = "+i+"回目"+ b);
+
 					// 盤外だった場合
-					if (a > 7 || b > 7) {
+					if (a > 7 || b > 7) 
 						break;
-					}
-						tool.turnStone(a,  b, stoneColor) ;
-					if(board[a][b] == stoneColor) {
-						break;
+					System.out.println("main.board[a][b]="+main.board[a][b]);
+
+					// Comが置こうとしてるマスが空であるかの条件分岐
+					if((main.board[a][b] != 1)&&(main.board[a][b] !=-1)) {
+						count += tool.turnLeftUp   (a, b, stoneColor);
+						count += tool.turnUp       (a, b, stoneColor);
+						count += tool.turnRightUp  (a, b, stoneColor);
+						count += tool.turnRight    (a, b, stoneColor);
+						count += tool.turnRightDown(a, b, stoneColor);
+						count += tool.turnDown     (a, b, stoneColor);
+						count += tool.turnLeftDown (a, b, stoneColor);
+						count += tool.turnLeft     (a, b, stoneColor);
+
+						System.out.println("Count = "+count);
+
+						// ターン終了判定
+						if(count > 0) {
+							break;
+						}
 					}
 				}
+
 				tool.printBoard(board);
 				tool.count(board);
+
+				turnFlag++;
+				System.out.println("turnFlag"+turnFlag);
+			}else {
+
+			// 入力値のチェック[ (0は正常値),X軸,Y軸 ]
+			int[] returnValue;
+
+			while(true) {
+				System.out.println("【コマの置く位置を決定してください】");
+				System.out.println("【　Ａ１～Ｈ８　？】");
+				System.out.println("【ｅｘ) Ｂ３　　　】");
+
+				String userInput = scanner.next();
+
+				returnValue = ivj.convertInputStone(userInput);
+				if(returnValue[0] == 0) 
+					break;
 			}
 
-				// 入力値のチェック[ (0は正常値),X軸,Y軸 ]
-				int[] returnValue;
-
-				while(true) {
-					System.out.println("【コマの置く位置を決定してください】");
-					System.out.println("【　Ａ１～Ｈ８　？】");
-					System.out.println("【ｅｘ) Ｂ３　　　】");
-
-					String userInput = scanner.next();
-
-					returnValue = ivj.convertInputStone(userInput);
-					if(returnValue[0] == 0) {
-						break;
-					}
-				}
-
-				int a = returnValue[1];
-				int b = returnValue[2];
+			a = returnValue[1];
+			b = returnValue[2];
 
 
-				if(checkValue == 1) {
-					stoneColor  = -1;
-				}else {
-					stoneColor  = 1;
-				}
-
-				tool.turnStone(a,  b,stoneColor) ;
-				tool.printBoard(board);
-				tool.count(board);
-			// コマの八方向の判定
-		
+			if(checkValue == 1) {
+				stoneColor  = -1;
+			}else {
+				stoneColor  = 1;
 			}
+			tool.turnStone(a, b, stoneColor);
+			tool.printBoard(board);
+			tool.count(board);
+			System.out.println("board[a][b]"+board[a][b]);
+
+			turnFlag++;
+			System.out.println("turnFlag"+turnFlag);	
+		}
+
+		}
 	}
 }
